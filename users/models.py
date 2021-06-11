108510e7cd0d88b1106cc7092df2234dd66cadda
Bukhosi Moyo <bukhosizimcode@gmail.com>
2021-06-11 21:13:35 +0200

    ✨ : Adding the authentification app

--- users/models.py
+++ users/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+
     is_first_year_student = models.BooleanField(default=False)
     is_transfare_student = models.BooleanField(default=False)
     is_education_proffessional = models.BooleanField(default=False)
     is_parent_or_adult = models.BooleanField(default=False)
+    
