061f68bb2d6036e9ccc02711ac4ff57b1d62e6a6
Bukhosi Moyo <bukhosizimcode@gmail.com>
2021-06-13 12:17:03 +0200

    🔥 :  Simplifying models Removing unnessessary code

--- student_profile/models.py
+++ student_profile/models.py
@@ -1,21 +1,9 @@
 from django.db import models
+from django.conf import settings 
 
 
-"""============================================Profile==========================================="""
 
-class Student(models.Model):
-    profile = models.ForeignKey(
-        "PersonalInformation", on_delete=models.CASCADE)
-    address = models.ForeignKey("Address", on_delete=models.CASCADE)
-    contact_details = models.ForeignKey(
-        "ContactDetail", on_delete=models.CASCADE)
-    demographics = models.ForeignKey("Demographic", on_delete=models.CASCADE)
-    language = models.ForeignKey("Language", on_delete=models.CASCADE)
-    citizenship = models.ForeignKey("Citizenship", on_delete=models.CASCADE)
-    
-    
-    class Meta:
-        verbose_name = "Profile (Parent)"
+"""============================================Student==========================================="""
 
 
 class PersonalInformation(models.Model):
@@ -25,6 +13,7 @@ class PersonalInformation(models.Model):
         ("Female", "Female"),
     )
 
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=100, blank=True, null=True)
     middle_name = models.CharField(max_length=100, blank=True, null=True)
     last_name = models.CharField(max_length=100, blank=True, null=True)
@@ -36,6 +25,9 @@ class PersonalInformation(models.Model):
 
     def __str__(self):
         return self.first_name
+
+    class Meta:
+        verbose_name_plural = "Personal Information"
 
 
 class ContactDetail(models.Model):
@@ -54,6 +46,7 @@ class ContactDetail(models.Model):
 
 
 class Address(models.Model):
+    related_field = models.ForeignKey(PersonalInformation, on_delete=models.CASCADE, null=True, blank=True)
     address = models.CharField(max_length=50, blank=True, null=True)
     city = models.CharField(max_length=50, blank=True, null=True)
     postal_code = models.CharField(max_length=10, blank=True, null=True)
@@ -63,19 +56,6 @@ class Address(models.Model):
     def __str__(self):
         return f"{self.address}"
 
-
-class Address2(models.Model):
-    related_address =models.ForeignKey(Address, null=True, blank=True, on_delete=models.CASCADE)
-    address = models.CharField(max_length=50, blank=True, null=True)
-    city = models.CharField(max_length=50, blank=True, null=True)
-    postal_code = models.CharField(max_length=10, blank=True, null=True)
-    country = models.CharField(max_length=30, blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.address}"
-
-    class Meta:
-        verbose_name_plural = "Address line 2"
     
 
 class Demographic(models.Model):
@@ -93,6 +73,7 @@ class Demographic(models.Model):
         ("Asian", "Asian"),
     )
 
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     religion = models.CharField(
         max_length=30, blank=True, null=True, choices=RELIGION)
     race = models.CharField(max_length=30, blank=True, null=True, choices=RACE)
@@ -137,7 +118,9 @@ class Citizenship(models.Model):
 
 """=======================================Family======================================"""
 
-class Household(models.Model):
+
+class Family(models.Model):
+
     RELATIONSHIP_STATUS = (
         ("Married", "Married"),
         ("Divorced", "Divorced"),
@@ -151,6 +134,7 @@ class Household(models.Model):
         ("Parent2", "Parent2"),
     )
 
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     relationship_status = models.CharField(
         max_length=20, blank=True, null=True, choices=RELATIONSHIP_STATUS)
     living_situation = models.CharField(
@@ -159,7 +143,11 @@ class Household(models.Model):
     number_of_children = models.IntegerField()
 
     def __str__(self):
-        return self.relationship_status
+        return f"{self.relationship_status}"
+
+
+    class Meta:
+        verbose_name_plural = "Family"
 
 
 class Parent(models.Model):
@@ -183,6 +171,7 @@ class Parent(models.Model):
         ("Other", "Other"),
     )
     
+    related_parent = models.ForeignKey(Family, on_delete=models.CASCADE, null=True, blank=True)
     parent_type = models.CharField(
         max_length=100, blank=True, null=True, choices=PARENT)
     parent_status = models.BooleanField(verbose_name="Is parent living?")
@@ -200,52 +189,7 @@ class Parent(models.Model):
     education_level = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
-        return self.parent_type
-
-
-class SecondParent(models.Model):
-    PARENT = (
-        ("Mother", "Mother"),
-        ("Father", "Father"),
-        ("Limited information", "I have limited information about this parent")
-    )
-
-    PREFIX = (
-        ("Dr.", "Dr"),
-        ("Ms.", "Ms."),
-        ("Mr.", "Mr."),
-        ("Mrs.", "Mrs."),
-    )
-
-    PHONE = (
-        ("Mobile", "Mobile"),
-        ("Home", "Home"),
-        ("Work", "Work"),
-        ("Other", "Other"),
-    )
-
-    related_parent = models.ForeignKey("Parent", on_delete=models.CASCADE, null=True, blank=True)
-    parent_type = models.CharField(
-        max_length=100, blank=True, null=True, choices=PARENT)
-    parent_status = models.BooleanField(verbose_name="Is parent living?")
-    parent_prefix = models.CharField(
-        max_length=10, blank=True, null=True, choices=PREFIX)
-    first_name = models.CharField(max_length=50, blank=True, null=True)
-    middle_name = models.CharField(max_length=50, blank=True, null=True)
-    last_name = models.CharField(max_length=50, blank=True, null=True)
-    birth_country = models.CharField(max_length=50, blank=True, null=True)
-    preferred_email = models.EmailField(max_length=40, blank=True, null=True)
-    preferred_phone = models.CharField(max_length=20, choices=PHONE)
-    phone_number = models.CharField(max_length=50, blank=True, null=True)
-    parent_address = models.CharField(max_length=50, blank=True, null=True)
-    occupation = models.CharField(max_length=50, blank=True)
-    education_level = models.CharField(max_length=50, blank=True, null=True)
-
-    def __str__(self):
-        return self.parent_type
-
-    class Meta:
-        verbose_name_plural = "Second Parent"
+        return f"{self.parent_type}"
 
 
 class Sibling(models.Model):
@@ -254,7 +198,7 @@ class Sibling(models.Model):
         ("Sister", "Sister"),
     )
 
-    related_parent = models.ForeignKey("Parent", on_delete=models.CASCADE, null=True, blank=True)
+    related_parent = models.ForeignKey(Family, on_delete=models.CASCADE, null=True, blank=True)
     first_name = models.CharField(max_length=50, blank=True, null=True)
     middle_name = models.CharField(max_length=50, blank=True, null=True)
     last_name = models.CharField(max_length=50, blank=True, null=True)
@@ -263,39 +207,43 @@ class Sibling(models.Model):
     education_level = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
-        return f"{self.number_of_Siblings}"
+        return f"{self.first_name}"[0]
 
 
 """=======================================Education======================================"""
 
-class School(models.Model):
+
+class Education(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     school_name = models.CharField(max_length=200, blank=True, null=True)
     date_of_entry = models.DateField()
     school_type = models.BooleanField()
     graduation_status = models.BooleanField()
     graduation_date = models.DateField(null=True, blank=True)
     progression_status = models.CharField(max_length=50, blank=True, null=True)
+    description = models.TextField(max_length=250, blank=True, null=True)
+
 
     def __str__(self):
         return self.school_name
 
+    class Meta:
+        verbose_name_plural = "Education"
 
-class OtherSchool(models.Model):
-    related_school = models.ForeignKey(School, on_delete=models.CASCADE)
+
+class School(models.Model):
+    related_school = models.ForeignKey(Education, on_delete=models.CASCADE)
     school_name = models.CharField(max_length=200, blank=True, null=True)
     date_started = models.DateField()
     date_finished = models.DateField()
-
-
-class SchoolDescription(models.Model):
-    related_school = models.ForeignKey(School, on_delete=models.CASCADE, null=True, blank=True)
     description = models.TextField(max_length=250, blank=True, null=True)
 
 
 class FuturePlan(models.Model):
+    related_school = models.ForeignKey(Education, on_delete=models.CASCADE)
     career_interest = models.CharField(max_length=50, blank=True, null=True)
     highest_degree = models.CharField(max_length=50, blank=True, null=True)
-    notoes = models.TextField(max_length=350, blank=True, null=True)
+    notes = models.TextField(max_length=350, blank=True, null=True)
 
     def __str__(self):
         return self.career_interest
@@ -304,10 +252,11 @@ class FuturePlan(models.Model):
 """=======================================Activity======================================"""
 
 class ActivityCheck(models.Model):
-    activity_interest = models.BooleanField(default=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    activity_interest = models.BooleanField(default=True, blank=True, null=True)
 
     def __str__(self):
-        return self.activity_interest
+        return f"{self.activity_interest}"
 
     
 class Activity(models.Model):
@@ -335,15 +284,15 @@ class Activity(models.Model):
         return self.activity_type
 
 
-
 """=======================================Writing======================================"""
 
 class PersonalEssay(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     consent = models.BooleanField(default=False)
     essay = models.TextField(max_length=800)
 
     def __str__(self):
-        return self.consent
+        return f"{self.consent}"
 
 
     class Meta:
@@ -367,8 +316,8 @@ class AdditionalInformation(models.Model):
     related_field = models.ForeignKey(PersonalEssay, on_delete=models.CASCADE, null=True, blank=True)
     any_disruptions = models.BooleanField()
     disruption = models.TextField(max_length=650, blank=True, null=True)
-    any_curcumstances = models.BooleanField()
+    any_curcumstance = models.BooleanField(default=False)
     circumstance = models.TextField(max_length=650, blank=True, null=True)
 
     def __str__(self):
-        return f"Disruptions {self.any_disruptions}, Curcumstances {self.any_circumstance}"
+        return f"Disruptions {self.any_disruptions}"
