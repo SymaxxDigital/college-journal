3309cebe5f2704e8a9a57715c5264e42d08cc0f6
Bukhosi Moyo <bukhosizimcode@gmail.com>
2021-06-16 07:56:16 +0200

    ✨ : Adding views for profile create and profile update

--- student_profile/views.py
+++ student_profile/views.py
@@ -1,3 +1,81 @@
-from django.shortcuts import render
+from .models import PersonalInformation
+from django.views.generic import ListView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from .forms import ContactDetailFormSet, AddressFormSet
 
-# Create your views here.
+
+class ProfileView(ListView):
+    """This will list all the available profiles"""
+    model = PersonalInformation
+    template_name = 'student_profile.html'
+    context_object_name = 'profiles'
+
+
+class ProfileCreateView(CreateView):
+    """This will be used to create the profiles for first time users"""
+    model = PersonalInformation
+    template_name = "student_profile_create.html"
+    success_url = reverse_lazy('student_profile:profile')
+    fields = "__all__"
+    #fields = ("first_name", "middle_name", "last_name", "nickname", "other_name", "sex", "other_gender", "date_of_birth", )
+
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileCreateView, self).get_context_data(**kwargs)
+
+        if self.request.POST:
+            context["contact_formset"] = ContactDetailFormSet(self.request.POST)
+            context["address_formset"] = AddressFormSet(self.request.POST)
+        else:
+            context["contact_formset"] = ContactDetailFormSet()
+            context["address_formset"] = AddressFormSet()
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data(form=form)
+        formset1 = context["contact_formset"]
+        formset2 = context["address_formset"]
+
+        if formset1.is_valid() and formset2.is_valid():
+            response = super().form_valid(form)
+            formset1.instance = self.object
+            formset2.instance = self.object
+            formset1.save()
+            formset2.save()
+            return response
+        else:
+            return super.form_valid(form)
+
+class ProfileUpdateView(UpdateView):
+    """This will be used to update or edit users information"""
+    model = PersonalInformation
+    template_name = "student_profile_update.html"
+    success_url = reverse_lazy('student_profile:profile')
+    fields = "__all__"
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileUpdateView, self).get_context_data(**kwargs)
+
+        if self.request.POST:
+            context["contact_formset"] = ContactDetailFormSet(self.request.POST, instance=self.object)
+            context["address_formset"] = AddressFormSet(self.request.POST, instance=self.object)
+        else:
+            context["contact_formset"] = ContactDetailFormSet(instance=self.object)
+            context["address_formset"] = AddressFormSet(instance=self.object)
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data(form=form)
+        formset1 = context["contact_formset"]
+        formset2 = context["address_formset"]
+
+        if formset1.is_valid() and formset2.is_valid():
+            response = super().form_valid(form)
+            formset1.instance = self.object
+            formset2.instance = self.object
+            formset1.save()
+            formset2.save()
+            return response
+        else:
+            return super.form_valid(form)
+
