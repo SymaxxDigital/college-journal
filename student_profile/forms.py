da837934d5093486f4ad946619739edec5662679
Bukhosi Moyo <bukhosizimcode@gmail.com>
2021-06-19 10:42:30 +0200

    ✨ : Adding the Demographic formset

--- student_profile/forms.py
+++ student_profile/forms.py
@@ -3,5 +3,11 @@ from . models import *
 
 
 """Personal Information"""
+
 ContactDetailFormSet = inlineformset_factory(PersonalInformation, ContactDetail, fields = ['related_field', 'phone_option', 'phone_number'], exclude = [], extra=1, can_delete = True)
 AddressFormSet = inlineformset_factory(PersonalInformation, Address, fields = ['address', 'city', 'postal_code', 'country'], exclude = [], extra=1, can_delete = True)
+
+"""Demographics"""
+
+LanguageFormSet = inlineformset_factory(Demographic, Language, fields = ['related_field', 'language', 'language_proficiency'], exclude = [], extra=1, can_delete = True)
+CitizenshipFormSet = inlineformset_factory(Demographic, Citizenship, fields = ['related_field', 'name'], exclude = [], extra=1, can_delete = True)
