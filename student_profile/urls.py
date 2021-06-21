b3110566faa6ee892d7457f16c78342e93330716
Bukhosi Moyo <bukhosizimcode@gmail.com>
2021-06-21 08:00:25 +0200

    ✨ : Addding routes for all the views

--- student_profile/urls.py
+++ student_profile/urls.py
@@ -1,10 +1,36 @@
 from django.urls import path
-from .views import ProfileView, ProfileCreateView, ProfileUpdateView
+from .views import (
+    ProfileView, 
+    ProfileCreateView, 
+    ProfileUpdateView, 
+    DemographicCreateView, 
+    DemographicUpdateView,
+    FamilyCreateView,
+    FamilyUpdateView,
+    EducationCreateView,
+    EducationUpdateView,
+    ActivityCreateView,
+    ActivityUpdateView,
+    PersonalEssayCreateView,
+    PersonalEssayUpdateView,
+
+
+)
 
 app_name = 'student_profile'
 
 urlpatterns = [
     path("", ProfileView.as_view(), name="profile"),
-    path("add/", ProfileCreateView.as_view(), name="profile_add"),
-    path("edit/<uuid:pk>/", ProfileUpdateView.as_view(), name="profile_edit"),
+    path("add/", ProfileCreateView.as_view(), name="profile_create"),
+    path("edit/<uuid:pk>/", ProfileUpdateView.as_view(), name="profile_update"),
+    path("demographic/add", DemographicCreateView.as_view(), name="demographic_create"),
+    path("demographic/edit/<uuid:pk>", DemographicUpdateView.as_view(), name="demographic_update"),
+    path("family/add", FamilyCreateView.as_view(), name="family_create"),
+    path("family/edit/<uuid:pk>", FamilyUpdateView.as_view(), name="family_update"),
+    path("education/add", EducationCreateView.as_view(), name="education_create"),
+    path("education/edit/<uuid:pk>", EducationUpdateView.as_view(), name="education_update"),
+    path("activity/add", ActivityCreateView.as_view(), name="activity_create"),
+    path("activity/edit/<uuid:pk>", ActivityUpdateView.as_view(), name="activity_update"),
+    path("personal-essay/add", PersonalEssayCreateView.as_view(), name="personal_essay_create"),
+    path("personal-essay/edit/<uuid:pk>", PersonalEssayUpdateView.as_view(), name="personal_essay_update"),
 ]
